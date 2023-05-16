ac0848d9caf57493e90c0731131f36cea5a3a812
Juan Sebastian Garcia <juansgperez@gmail.com>
2023-05-16 17:28:38 -0500

    Reestructuración de metodos y clases - Metodo crear_categoria agregado

--- arquitectura/modelo.py
+++ arquitectura/modelo.py
@@ -1,15 +1,13 @@
 import json
-from abc import ABC, abstractmethod
 
 
-class Usuario(ABC):
-    def __init__(self, nombre, numero_id, contrasena, cargo):
+class Usuario:
+    def __init__(self, nombre: str, numero_id: str, contrasena: str, cargo: str):
         self.nombre: str = nombre
         self.numero_id: str = numero_id
         self.contrasena: str = contrasena
         self.cargo: str = cargo
 
-    @abstractmethod
     def iniciar_sesion(self, numero_id, contrasena):
         with open("usuarios.json", "r", encoding="utf-8") as archivo:
             datos = json.load(archivo)
@@ -19,15 +17,6 @@ class Usuario(ABC):
                     return  # Si encuentra una coincidencia, termina la función aquí
             # Si llega hasta aquí, significa que no encontró ninguna coincidencia
             print("El usuario o contraseña no coinciden")
-
-    @abstractmethod
-    def mostrar_inventario(self, producto):
-        pass
-
-
-class Administrador(Usuario):
-    def __init__(self, nombre, numero_id, contrasena):
-        super().__init__(nombre, numero_id, contrasena, 'adminsitrador')
 
     def registrar_usuario(self, nombre, numero_id, contrasena, cargo):
 
@@ -39,11 +28,6 @@ class Administrador(Usuario):
         with open("usuarios.json", "r", encoding="utf-8") as archivo:
             datos = json.load(archivo)
 
-            # Excepción
-            for usuario in datos:
-                if usuario["numero_id"] == numero_id:
-                    raise Exception
-
             nuevo_usuario["nombre"] = nombre
             nuevo_usuario["numero_id"] = numero_id
             nuevo_usuario["contrasena"] = contrasena
@@ -53,55 +37,38 @@ class Administrador(Usuario):
         with open("usuarios.json", "w", encoding="utf-8") as archivo:
             json.dump(datos, archivo, indent=1)
 
-    def iniciar_sesion(self, numero_id, contrasena):
-        super().iniciar_sesion(numero_id, contrasena)
-
-    def mostrar_inventario(self, producto):
-        super().mostrar_inventario(producto)
-
     def consultar_facturas(self, facturas):
         pass
 
-    def calcular_precio(self, producto, cantidad):
-        pass
-
-
-class Vendedor(Usuario):
-
-    def __init__(self, nombre, numero_id, contrasena):
-        super().__init__(nombre, numero_id, contrasena, 'vendedor')
-
-    def iniciar_sesion(self, numero_id, contrasena):
-        super().iniciar_sesion(numero_id, contrasena)
-
     def mostrar_inventario(self, producto):
-        super().mostrar_inventario(producto)
-
-    def calcular_precio(self, producto, cantidad):
         pass
 
-    def generar_factura(self, nombre_cliente, id_producto, cantidad, subtotal):
-        pass
+    def calcular_precio(self, id_producto, cantidad):
+        producto = self.inventario.get(id_producto)
+        # Calculamos el precio
+        precio = producto.precio * cantidad
+        return precio
 
-
-class EncargadoBodega(Usuario):
-
-    def __init__(self, nombre, numero_id, contrasena):
-        super().__init__(nombre, numero_id, contrasena, 'Encargadobodega')
-
-    def iniciar_sesion(self, numero_id, contrasena):
-        super().iniciar_sesion(numero_id, contrasena)
+    def generar_factura(self, nombre_cliente, id_producto, cantidad):
+        # Calculamos el subtotal
+        subtotal = self.calcular_precio(id_producto, cantidad)
+        # Creamos la factura
+        factura = {
+            "nombre_cliente": nombre_cliente,
+            "id_producto": id_producto,
+            "cantidad": cantidad,
+            "subtotal": subtotal
+        }
+        # Guardamos la factura en un archivo .json
+        with open(f'{nombre_cliente}_factura.json', 'w') as f:
+            json.dump(factura, f)
+        return factura
 
     def crear_categoria(self, nombre_categoria, precio):
-        nueva_categoria = {"nombre categoria": nombre_categoria, "precio": precio, "productos": []}
+        nueva_categoria = {"nombre_categoria": nombre_categoria, "productos": [], "precio": precio}
 
         with open("inventario.json", "r", encoding="utf-8") as archivo:
             datos = json.load(archivo)
-
-            # Excepción
-            for categoria in datos:
-                if categoria["nombre categoria"] == nombre_categoria:
-                    raise Exception
 
             nueva_categoria["nombre_categoria"] = nombre_categoria
             nueva_categoria["precio"] = precio
@@ -119,9 +86,6 @@ class EncargadoBodega(Usuario):
     def modificar_producto(self, otro):
         pass
 
-    def mostrar_inventario(self, producto):
-        super().mostrar_inventario(producto)
-
 
 class Factura:
 
@@ -130,15 +94,16 @@ class Factura:
         self.cantidad: int = cantidad
 
 
-class Categoria:
-
-    def __init__(self, nombre_categoria, precio_base):
-        self.nombre_categoria: str = nombre_categoria
-        self.precio_base: float = precio_base
+class Inventario:
+    pass
 
 
-class Producto:
+bodego = Usuario("sebas", "12", "123", "bodega")
 
-    def __init__(self, id_producto, cantidad, categoria):
-        pass
+while True:
+    dec = input("1 ingresar categoria")
+    if dec == "1":
+        nombre = input("Nombre categoria: ")
+        precio = input("precio categoria")
+        bodego.crear_categoria(nombre, precio)
 
